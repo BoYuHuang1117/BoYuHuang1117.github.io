d2802a5295515872160d95d918f63b9ac419b6e1
BoYu Huang <boyuhuang.michael@gmail.com>
2024-05-19 22:48:02 -0500

    Draft on content

--- src/components/MiddleMain.vue
+++ src/components/MiddleMain.vue
@@ -1,8 +1,20 @@
 <template>
     <main>
-      <h2>About Me</h2>
-      <p>I am a basketball enthusiast and a genuine sports fan.</p>
-      <!-- Add more content here -->
+      <h1>Achievement</h1>
+      <li>Silver Award of 2019 IFMBE</li>
+      <h1>Publications</h1>
+      <li>Improvement of Environment and Camera Setting on Extraction of Heart Rate Using Eulerian Video Magnification</li>
+      <li>A Finite Element Analysis Simulation of Patellar and Quadriceps Tendon injury during Squatting</li>
+      <h1>Area of Interests</h1>
+      <li>Basketball Analysis</li>
+      <li></li>
+      <li>Backend Software Engineering</li>
+      <li>Natural Language Processing and LLM</li>
+      <h1>About Me</h1>
+      <p>I am a genuine sports fan who plays basketball, pickleball and tennis.</p>
+      <li></li>
+      
+
     </main>
   </template>
   
