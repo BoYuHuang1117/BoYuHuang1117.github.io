49fcd6389352a3a43d0ea090f8c6aca4e52890be
BoYu Huang <boyuhuang.michael@gmail.com>
2024-06-08 11:34:01 -0500

    Restruct and add few more links and icons

--- src/components/MiddleMain.vue
+++ src/components/MiddleMain.vue
@@ -1,10 +1,15 @@
 <template>
   <main>
+    <h1>Introduction</h1>
+    <p>I am a genuine sports fan who plays basketball, pickleball and tennis who loves sci-fi movies and animations.</p>
     
     <h1>Travel Across the States</h1>
+    <li>May 2024 - Indianapolis, IN</li>
+    <li>May 2024 - Milwaukee, WI (Aurora light)</li>
+    <li>May 2024 - Holland, MI</li>
     <li>Feb 2024 - Phoenix, AZ</li>
     <li>Jul 2023 - Denver CO</li>
-    <li>May 2023 - Minneapolism MN</li>
+    <li>May 2023 - Minneapolis MN</li>
     <li>Mar 2023 - Seatle, WA </li>
     <li>Sep 2022 - Los Angeles, CA</li>
     <li>Dec 2021 - Houston, TX</li>
@@ -16,17 +21,22 @@
     <li>Silver Award of 2019 ICBHI</li>
     <img src="../assets/ICBHI-onStage.jpg" alt="on stage" style="width: 400px;height: 300px;">
     <img src="../assets/ICBHI-solo.jpg" alt="solo" style="width: 300px;height: 400px;">
+    
     <h1>Publications</h1>
-    <li><a href="https://link.springer.com/chapter/10.1007/978-3-030-30636-6_52"> Improvement of Environment and Camera Setting on Extraction of Heart Rate Using Eulerian Video Magnification</a></li>
-    <li><a href="https://www.semanticscholar.org/paper/An-Finite-Element-Analysis-of-Patellar-and-Tendon-Huang-Hsu/bd83636e6fd716d94296545a6fe52e1976392e5d"> A Finite Element Analysis Simulation of Patellar and Quadriceps Tendon injury during Squatting</a></li>
+    <li><a href="https://link.springer.com/chapter/10.1007/978-3-030-30636-6_52" target="_blank" rel="noopener noreferrer"> Improvement of Environment and Camera Setting on Extraction of Heart Rate Using Eulerian Video Magnification</a></li>
+    <li><a href="https://www.semanticscholar.org/paper/An-Finite-Element-Analysis-of-Patellar-and-Tendon-Huang-Hsu/bd83636e6fd716d94296545a6fe52e1976392e5d" target="_blank" rel="noopener noreferrer"> A Finite Element Analysis Simulation of Patellar and Quadriceps Tendon injury during Squatting</a></li>
+    
     <h1>Area of Interests</h1>
     <li>Basketball Analysis <font-awesome-icon icon="fa-solid fa-basketball" /></li>
     <li>Pickleball lover <font-awesome-icon icon="fa-solid fa-table-tennis-paddle-ball" /></li>
     <li>Backend Software Engineering <font-awesome-icon icon="fa-solid fa-laptop" /></li>
     <li>Natural Language Processing and LLM <font-awesome-icon icon="fa-solid fa-language" /></li>
-    <h1>About Me</h1>
-    <p>I am a genuine sports fan who plays basketball, pickleball and tennis who loves sci-fi movies and animations.</p>
-      
+    
+    <h1>More about Me</h1>
+    <li>Location: Chicago-Milwaukee area <font-awesome-icon icon="fa-solid fa-location-crosshairs" /></li>
+    <li><a href="https://www.linkedin.com/in/bo-yuhuang-michael/" target="_blank" rel="noopener noreferrer"> Linkedin Profile </a><font-awesome-icon icon="fa-brands fa-linkedin" /></li>
+    <li><a href="https://github.com/BoYuHuang1117" target="_blank" rel="noopener noreferrer"> Github Profile </a><font-awesome-icon icon="fa-brands fa-github" /></li>
+    <li><a href="https://leetcode.com/u/michael_byhuang/" target="_blank" rel="noopener noreferrer"> Leetcode Profile </a><font-awesome-icon icon="fa-solid fa-code" /></li>
 
     </main>
   </template>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 /* import the fontawesome core and icon component */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBasketball, faTableTennisPaddleBall, faLaptop, faLanguage } from '@fortawesome/free-solid-svg-icons'
-library.add(faBasketball, faTableTennisPaddleBall, faLaptop, faLanguage)
+import { faBasketball, faTableTennisPaddleBall, faLaptop, faLanguage, faCode, faLocation } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+library.add(faBasketball, faTableTennisPaddleBall, faLaptop, faLanguage, faCode, faLocation)
+library.add(faLinkedin, faGithub)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
