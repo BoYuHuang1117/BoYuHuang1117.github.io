aa461c2e65c17169134eb91ccfcb091a123d7d73
BoYu Huang <boyuhuang.michael@gmail.com>
2024-06-23 15:56:22 -0500

    Adding router capability

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
+  <HeaderMain />
   <div id="app">
-    <HeaderMain />
-    <MiddleMain />
-    <FooterMain />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/photo">Photo</router-link> |
+    <router-link to="publication">Publications and Awards</router-link>
+    <router-view />
   </div>
+  <FooterMain />
 </template>
 
 <script>
-import HeaderMain from './views/HeaderMain.vue';
-import MiddleMain from './views/MiddleMain.vue';
-import FooterMain from './views/FooterMain.vue';
+import HeaderMain from '@/components/HeaderMain.vue';
+import FooterMain from '@/components/FooterMain.vue';
 
 export default {
   name: 'App',
   components: {
     HeaderMain,
-    MiddleMain,
-    FooterMain,
+    FooterMain
   },
 };
 </script>
