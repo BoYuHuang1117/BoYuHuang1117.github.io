18ee7c57a7179171163030a9e84bd0149b3a0a8d
BoYu Huang <38172621+BoYuHuang1117@users.noreply.github.com>
2024-07-20 12:37:00 -0500

    Add expand capability for interests content (#9)

--- src/components/MiddleMain.vue
+++ src/components/MiddleMain.vue
@@ -13,13 +13,17 @@
     </div>
   </main>
 
-  <content>
-    <h1>Area of Interests</h1>
-    <li>Basketball Analysis <font-awesome-icon icon="fa-solid fa-basketball" /></li>
-    <li>Pickleball lover <font-awesome-icon icon="fa-solid fa-table-tennis-paddle-ball" /></li>
-    <li>Backend Software Engineering <font-awesome-icon icon="fa-solid fa-laptop" /> <font-awesome-icon icon="fa-solid fa-database" /> <font-awesome-icon icon="fa-solid fa-server" /></li>
-    <li>Natural Language Processing and LLM <font-awesome-icon icon="fa-solid fa-language" /></li>
-    
+  <h1>Area of Interests
+    <font-awesome-icon icon="fa-solid fa-basketball" /><font-awesome-icon icon="fa-solid fa-table-tennis-paddle-ball" />
+    <font-awesome-icon icon="fa-solid fa-laptop" /> <font-awesome-icon icon="fa-solid fa-server" /> <font-awesome-icon icon="fa-solid fa-database" /> <font-awesome-icon icon="fa-solid fa-language" />
+  </h1>
+  <interest-row v-for="(interest, index) in interests" :key="index" :interest="interest">
+    <template v-for="(item, index) in interest.items" :key="index">
+      <p> - {{ item }}</p>
+    </template>
+  </interest-row>
+  
+  <content>  
     <h1>More about Me</h1>
     <li>Location: Chicago-Milwaukee area <font-awesome-icon icon="fa-solid fa-location-crosshairs" /></li>
     <li><a href="https://www.linkedin.com/in/bo-yuhuang-michael/" target="_blank" rel="noopener noreferrer"> Linkedin Profile </a><font-awesome-icon icon="fa-brands fa-linkedin" /></li>
@@ -29,9 +33,40 @@
 </template>
   
 <script>
-  export default {
+  import InterestRow from "./Interests.vue"
+  import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
     name: 'MiddleMain',
-  };
+    components: { InterestRow },
+    setup() {
+      const interests = ref([
+        {
+          key: 'Basketball Analysis',
+          icons: ['fa-solid fa-basketball'],
+          items: ['Small forward', 'Golden State Warriors fans',],
+        },
+        {
+          key: 'Pickleball lover',
+          icons: ['fa-solid fa-table-tennis-paddle-ball'],
+          items: ['Kitchen (non-valley zone)', 'Newbie'],
+        },
+        {
+          key: 'Backend Software Engineering',
+          icons: ['fa-solid fa-laptop', 'fa-solid fa-database', 'fa-solid fa-server'],
+          items: ['Python Flask', 'MySQL', 'Celery asynchronous task', 'Docker'],
+        },
+        {
+          key: 'Natural Language Processing and LLM',
+          icons: ['fa-solid fa-language'],
+          items: ['Bert', 'Transfer Learning', 'LSTM', 'Recurrent Neural Network (RNN)'],
+        }
+      ]);
+      return {
+        interests,
+      };
+    },
+  });
 </script>
 
 <style scoped>
