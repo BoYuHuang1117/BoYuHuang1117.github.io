91017ab39bf0516310bffa9083dd045447d4946f
BoYu Huang <38172621+BoYuHuang1117@users.noreply.github.com>
2024-08-05 20:59:01 -0500

    Handle click event in parent component using emit in setup (#11)

---
+++ src/components/InterestRow.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="list-item-container">
+      <div class="list-item-head p-8">
+        <li>{{ interest.key }}
+          <span
+            class="expand"
+            @click="handleClick"
+            v-text="toggletText[isExpanded ? 1 : 0]"
+          ></span>
+        </li>
+      </div>
+      <div class="list-item-body p-8" v-show="isExpanded">
+        <slot />
+      </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'InterestRow',
+  props: {
+    interest: Object,
+    isExpanded: Boolean
+  },
+  setup(props, { emit }) {
+    const toggletText = ["More", "Hide"];
+
+    const handleClick = () => {
+      emit('toggle');
+    };
+
+    return { toggletText, handleClick };
+  }
+});
+</script>
+
+<style scoped>
+  li {
+    margin-bottom: 10px;
+    background-color: lightskyblue;
+    padding: 6px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+  .expand {
+    font-weight: 100;
+    text-align: right;
+    float: right;
+  }
+</style>
--- src/components/InterestSection.vue
+++ src/components/InterestSection.vue
@@ -1,46 +1,61 @@
 <template>
-  <interest-row v-for="(interest, index) in interests" :key="index" :interest="interest">
-    <template v-for="(item, index) in interest.items" :key="index">
-      <p> - {{ item }}</p>
-    </template>
-  </interest-row>
+  <div>
+    <InterestRow
+      v-for="(interest, index) in interests"
+      :key="index"
+      :interest="interest"
+      :isExpanded="expandedIndex === index"
+      @toggle="handleToggle(index)"
+    >
+      <template v-for="(item, index) in interest.items" :key="index">
+        <p> - {{ item }} </p>
+      </template>
+    </InterestRow>
+  </div>
 </template>
 
 <script>
-import InterestRow from "./Interests.vue"
+import InterestRow from "./InterestRow.vue";
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-    name: 'InterestSection',
-    components: { InterestRow },
-    setup() {
-      const interests = ref([
-        {
-          key: 'Basketball Analysis',
-          icons: ['fa-solid fa-basketball'],
-          items: ['Small forward', 'Golden State Warriors fans',],
-        },
-        {
-          key: 'Pickleball lover',
-          icons: ['fa-solid fa-table-tennis-paddle-ball'],
-          items: ['Kitchen (non-valley zone)', 'Newbie'],
-        },
-        {
-          key: 'Backend Software Engineering',
-          icons: ['fa-solid fa-laptop', 'fa-solid fa-database', 'fa-solid fa-server'],
-          items: ['Python Flask', 'MySQL', 'Celery asynchronous task', 'Docker'],
-        },
-        {
-          key: 'Natural Language Processing and LLM',
-          icons: ['fa-solid fa-language'],
-          items: ['Bert', 'Transfer Learning', 'LSTM', 'Recurrent Neural Network (RNN)'],
-        }
-      ]);
-      return {
-        interests,
-      };
-    },
-  });
+  name: 'InterestSection',
+  components: { InterestRow },
+  setup() {
+    const interests = ref([
+      {
+        key: 'Basketball Analysis',
+        icons: ['fa-solid fa-basketball'],
+        items: ['Small forward', 'Golden State Warriors fans'],
+      },
+      {
+        key: 'Pickleball lover',
+        icons: ['fa-solid fa-table-tennis-paddle-ball'],
+        items: ['Kitchen (non-valley zone)', 'Newbie'],
+      },
+      {
+        key: 'Backend Software Engineering',
+        icons: ['fa-solid fa-laptop', 'fa-solid fa-database', 'fa-solid fa-server'],
+        items: ['Python Flask', 'MySQL', 'Celery asynchronous task', 'Docker'],
+      },
+      {
+        key: 'Natural Language Processing and LLM',
+        icons: ['fa-solid fa-language'],
+        items: ['Bert', 'Transfer Learning', 'LSTM', 'Recurrent Neural Network (RNN)'],
+      }
+    ]);
 
+    const expandedIndex = ref(null);
+
+    const handleToggle = (index) => {
+      expandedIndex.value = expandedIndex.value === index ? null : index;
+    };
+
+    return {
+      interests,
+      expandedIndex,
+      handleToggle
+    };
+  },
+});
 </script>
-
