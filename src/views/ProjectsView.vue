4781ffd3bf9027a2b9529a9118b458c1c9848d2e
BoYu Huang <38172621+BoYuHuang1117@users.noreply.github.com>
2024-09-08 14:26:51 -0500

    Add one new project description (#17)
    * Add three supporting imgs
    
    * Add main content in project page
    
    * modify img position

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,7 +4,22 @@
         <li> Adjustable window size </li>
         <li> Play with cursor </li>
         
-        <ImageEnlarge src="images/tetris.gif"></ImageEnlarge>
+        <ImageEnlarge src="images/tetris.gif" pos="center"></ImageEnlarge>
+
+        <h1> Mandarin version of Amazon "Alexa" (雅婷姊) <a href="https://github.com/BoYuHuang1117/Audio-Keyword-Spotting-in-Chinese" target="_blank" rel="noopener noreferrer"> Link </a> </h1>
+        <li> In data preprocessing folder, voice processing includes adding background noise and generate mel frequency cepstrum (MFCC) </li>
+        <br>
+        <li> In main training folder (./code), it demonstrates the loss vs. epoch graph, sample test case and model hyper-parameters summary </li>
+        <ImageEnlarge src="images/loss-vs-epoch.png" pos="center"></ImageEnlarge>
+        <ImageEnlarge src="images/sample-audio.png" width=400 height=500 pos="center"></ImageEnlarge>
+        <br><br>
+        <li> Model architecture and hyper parameters </li>
+        <ImageEnlarge src="images/model-detail.png" width=350 height=450 pos="center"></ImageEnlarge>
+        <br><br>
+        <li> To play with the app, a simple terminal control version is provided in ./UI folder </li>
+        <p> - Install requirements for Python </p>
+        <p> - Follow the prompt in terminal </p>
+        <p> - Speak out the awakening keyword "Yating Jie" (雅婷姊) </p>
     </main>
 </template>
 
@@ -27,5 +42,8 @@
   main { 
     text-align: left;
     background-color: lightgrey;
+  }
+  h1 {
+    text-align: center;
   }
 </style>
